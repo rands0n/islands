032653a7c52450bafd3510563f2b45c5162a0574
Randson <orandson@gmail.com>
2020-06-28 16:26:30 -0300

    feat(rules): include player1 turn

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -33,6 +33,20 @@ defmodule IslandsEngine.Rules do
     end
   end
 
+  def check(%Rules{state: :player1_turn} = rules, {:guess_coordinate, :player1}) do
+    {:ok, %Rules{rules | state: :player2_turn}}
+  end
+
+  def check(%Rules{state: :player1_turn} = rules, {:win_check, win_or_not}) do
+    case win_or_not do
+      :no_win ->
+        {:ok, rules}
+
+      :win ->
+        {:ok, %{rules | state: :game_over}}
+    end
+  end
+
   def check(_state, _action), do: :error
 
   defp both_players_islands_set?(rules) do
